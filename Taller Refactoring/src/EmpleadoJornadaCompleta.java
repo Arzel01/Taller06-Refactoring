8f8370a9fb534eea2571701737f7bbf012173f49
Leonardo Macias <105889369+leodamac@users.noreply.github.com>
2024-12-12 21:32:52 -0500

    Update EmpleadoJornadaCompleta.java

--- Taller Refactoring/src/EmpleadoJornadaCompleta.java
+++ Taller Refactoring/src/EmpleadoJornadaCompleta.java
@@ -1,1 +1,9 @@
 
+public abstract class EmpleadoJornadaCompleta extends Empleado {
+    private double salarioBase;
+    
+    public EmpleadoFijo(String nombre, double salarioBase, int horasTrabajadas, Departamento departamento, double bonoAnual, String genero) {
+        super(nombre, horasTrabajadas, departamento, genero);
+        this.salarioBase = salarioBase;
+        
+    }
