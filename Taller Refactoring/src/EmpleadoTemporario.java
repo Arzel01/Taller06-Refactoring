dc64ccc85d8efbf2e23373999ca44b713b2b4053
Leonardo Macias <105889369+leodamac@users.noreply.github.com>
2024-12-12 21:39:20 -0500

    Update EmpleadoTemporario.java

--- Taller Refactoring/src/EmpleadoTemporario.java
+++ Taller Refactoring/src/EmpleadoTemporario.java
@@ -1,18 +1,9 @@
-public class EmpleadoTemporario extends Empleado {
-    private String nombre;
-    
-    private double salarioBase;
-    private int horasTrabajadas;
-    private String departamento;
+public class EmpleadoTemporario extends EmpleadoJornadaCompleta {
     private int mesesContrato;
 
     public EmpleadoTemporario(String nombre, double salarioBase, int horasTrabajadas, Departamento departamento, int mesesContrato, String genero) {
-        this.nombre = nombre;
-        this.salarioBase = salarioBase;
-        this.horasTrabajadas = horasTrabajadas;
-        this.departamento = departamento;
+        super(nombre, salarioBase, horasTrabajadas, departamento, genero);
         this.mesesContrato = mesesContrato;
-        this.genero = genero;
     }
 
     public void imprimirDetalles() {
